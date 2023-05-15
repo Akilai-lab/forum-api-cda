aee91c228d0aa0c61949155d04ea4bfbfbf71bff
Marina Perez <akilaroha@hotmail.fr>
2023-05-15 22:33:05 +0200

    modification de noms de tables

--- Forum-project-staging/BackEnd/apiForumBack/apiFolder/src/Entity/Comments.php
+++ Forum-project-staging/BackEnd/apiForumBack/apiFolder/src/Entity/Comments.php
@@ -6,7 +6,7 @@ use App\Repository\CommentsRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentsRepository::class)]
-#[ORM\Table(name: 'commentToPost')]
+#[ORM\Table(name: 'comments')]
 class Comments
 {
     #[ORM\Id]
--- Forum-project-staging/BackEnd/apiForumBack/apiFolder/src/Entity/Forum.php
+++ Forum-project-staging/BackEnd/apiForumBack/apiFolder/src/Entity/Forum.php
@@ -6,7 +6,7 @@ use App\Repository\ForumRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ForumRepository::class)]
-#[ORM\Table(name: 'forumPost')]
+#[ORM\Table(name: 'forum')]
 class Forum
 {
     #[ORM\Id]
